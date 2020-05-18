05183d6b909da76a4a8b7a4ca3ea66adbb8a06d1
lihaoquan <lihaoquan.dev@gmail.com>
2020-05-19 00:33:52 +0800

    Revert "Revert "Revert "Set console to output unicode, to be avoided on other platforms."""
    This reverts commit b56d8b23b09d67df112642f921072e5905878130.

--- src/Operations/Hiragana.h
+++ src/Operations/Hiragana.h
@@ -17,19 +17,19 @@ public:
 
 	enum class row { agyou_row, kagyou_row, sagyou_row, tagyou_row };
 
-	wchar_t* GetRandomHiragana(row row, int random);
+	std::string GetRandomHiragana(row row, int random);
 private:
 	bool gameOngoing = true;
 
-	wchar_t* agyou[5] = {L"あ", L"い", L"う", L"え", L"お"};
+	std::string agyou[5] = {"あ", "い", "う", "え", "お"};
 	std::string agyouSound[5] = { "a", "i", "u", "e", "o" };
 
-	wchar_t* kagyou[5] = { L"か", L"き", L"く", L"け", L"こ" };
+	std::string kagyou[5] = { "か", "き", "く", "け", "こ" };
 	std::string kagyouSound[5] = { "ka", "ki", "ku", "ke", "ko" };
 
-	wchar_t* sagyou[5] = { L"さ", L"し", L"す", L"せ", L"そ"};
+	std::string sagyou[5] = { "さ", "し", "す", "せ", "そ"};
 	std::string sagyouSound[5] = { "sa", "shi", "su", "se", "so" };
 
-	wchar_t* tagyou[5] = { L"た", L"ち", L"つ", L"て", L"と" };
+	std::string tagyou[5] = { "た", "ち", "つ", "て", "と" };
 	std::string tagyouSound[5] = { "ta", "chi", "tsu", "te", "to" };
 };
